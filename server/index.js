19e8b600bac6759c6ab0542edf61567ab02875a1
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-03-12 01:11:08 +0000

    WIP

--- server/index.js
+++ server/index.js
@@ -21,19 +21,33 @@ let db = mysql.createConnection({
 db.connect();
 
 function authenticateViaCredentials(email, password, fingerprintingResults, res) {
-    let sql = `SELECT COUNT(*) AS 'status' FROM authentication WHERE email = "${email}" AND password = "${password}";`;
+    let sql = `SELECT * FROM authentication WHERE email = "${email}" AND password = "${password}";`;
+
     db.query(sql, function (error, results, fields) {
-        if (results[0].status) {
+        //console.log(results[0].id);
+        if (results[0]) {
             res.status = 200;
-            res.send({
-                status: "success",
-                matched: {
-                    percentage: 100,
-                    hash: true,
-                    canvasHash: true,
-                    otherFields: []
+
+            let sqlFP = `SELECT * FROM fingerprint WHERE authentication_id = '${results[0].id}';`;
+            //console.log(sqlFP);
+
+            db.query(sqlFP, function (_error, _results, _fields) {
+                if (_results[0]) {
+                    //console.log(_results[0].fingerprint);
+
+                    res.send({
+                        status: "success",
+                        matched: matchHash(fingerprintingResults, _results[0].fingerprint)
+                    });
+                } else {
+                    res.send({
+                        status: "success",
+                        matched:(matchHash(fingerprintingResults, ""))
+                    })
                 }
+
             });
+
         } else {
             res.status = 400;
             res.send({
@@ -52,9 +66,38 @@ function authenticateViaCredentials(email, password, fingerprintingResults, res)
     });
 }
 
+function matchHash(liveHash, dbHash) {
+    let percentageMatch = 0;
+    let hashMatch = false;
+
+    //liveHash = liveHash.trim();
+    //dbHash = dbHash.trim();
+    //liveHash = liveHash.replace("' ","");
+
+    hashMatch = (liveHash == dbHash);
+
+    console.log("live:",liveHash);
+    console.log(dbHash);
+
+    /*liveHash = JSON.parse(liveHash);
+    //dbHash = JSON.parse(dbHash);
+
+    if (liveHash.hash && dbHash.hash) {
+        return liveHash.hash == dbHash.hash;
+    }*/
+
+    let match = {
+        percentage: percentageMatch,
+        hash: hashMatch,
+        canvasHash: true,
+        otherFields: ["Hello World"]
+    };
+    return match;
+}
+
 app.post("/", function(req, res) {
-    console.log("hello");
-    console.log(req.body);
+    //console.log("hello");
+    //console.log(req.body);
 
     let email = req.body.email;
     let password = req.body.password;
