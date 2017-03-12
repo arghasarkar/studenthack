bfee7ff716b084be7e5081bcc9fab218b92a46db
gmusa1 <gisimusa98@gmail.com>
2017-03-12 09:45:59 +0100

    worked on UI

--- loginpage/index.js
+++ loginpage/index.js
@@ -7,38 +7,46 @@ function getCanvasHash() {
 
     new Fingerprint2().get(function(result, components){
 
-        
+
         console.log(result); //a hash, representing your device fingerprint
         console.log(components); // an array of FP components
     });
 }
 
 
-function main()
-{
-    var e= returnEmail();
-    var p= returnPass();
-    var url="http://10.99.155.2:4000/";
+function main() {
+    var e = returnEmail();
+    var p = returnPass();
+    var url = "http://10.99.155.2:4000/";
     console.log(url);
     $.post(url, {
-            email:e,
-            password:p
+            email: e,
+            password: p
         },
         function (data) {
-           console.log(data);
+            console.log(data);
+            console.log("matched percentage:", data.matched.percentage);
+            console.log('canvas match', data.matched.canvasHash);
+            document.getElementById("percentageValue").innerHTML = data.matched.percentage;
+            var otherfields = data.matched.otherFields;
+
+            document.getElementById("canvasHash").innerHTML = data.matched.canvasHash;
+
         });
 
-
-    fetch(url).then(function(response) {
-        console.log('Status:', response.status)
-
-        response.json().then(function(data){
-            console.log('FETCH Result:', data);
-        }).catch(function(err){
-            console.log('FETCH Parsing Error', err)
-        });
-    });
-
+    for (i=0; i<otherFields.length ; i++)
+    {
+        document.getElementById("otherFields").innerHTML = data.matched.otherFields;
+    }
+    // fetch(url).then(function(response) {
+    //     console.log('Status:', response.status)
+    //
+    //     response.json().then(function(data){
+    //         console.log('FETCH Result:', data);
+    //     }).catch(function(err){
+    //         console.log('FETCH Parsing Error', err)
+    //     });
+    // /*});*/
 
 
 }
@@ -50,4 +58,6 @@ function returnEmail(){
 function returnPass(){
     return document.getElementById('pwd').value;
 }
+
+
 
