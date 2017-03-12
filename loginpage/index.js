8c6e535ea255c7b8a4c2284d72d1ca3b96632263
gmusa1 <gisimusa98@gmail.com>
2017-03-12 12:12:12 +0100

    wip

--- loginpage/index.js
+++ loginpage/index.js
@@ -5,48 +5,83 @@ getCanvasHash();
 function getCanvasHash() {
     "use strict";
 
-    new Fingerprint2().get(function(result, components){
+   return  new Fingerprint2().get(function(result, components){
 
-
+       return {
+            hash: result,
+           result: components
+       };
+/*
         console.log(result); //a hash, representing your device fingerprint
-        console.log(components); // an array of FP components
+        console.log(components); // an array of FP components*/
     });
 }
+
 
 
 function main() {
     var e = returnEmail();
     var p = returnPass();
     var url = "http://10.99.155.2:4000/";
-    console.log(url);
-    $.post(url, {
-            email: e,
-            password: p
-        },
-        function (data) {
-            console.log(data);
-            console.log("matched percentage:", data.matched.percentage);
-            console.log('canvas match', data.matched.canvasHash);
-            document.getElementById("percentageValue").innerHTML = data.matched.percentage;
-            var otherfields = data.matched.otherFields;
+    //console.log(url);
 
-            document.getElementById("canvasHash").innerHTML = data.matched.canvasHash;
 
-        });
 
-    for (i=0; i<otherFields.length ; i++)
-    {
-        document.getElementById("otherFields").innerHTML = data.matched.otherFields;
-    }
-    // fetch(url).then(function(response) {
-    //     console.log('Status:', response.status)
-    //
-    //     response.json().then(function(data){
-    //         console.log('FETCH Result:', data);
-    //     }).catch(function(err){
-    //         console.log('FETCH Parsing Error', err)
-    //     });
-    // /*});*/
+    new Fingerprint2({excludeJsFonts: true}).get(function(result, components){
+
+        var x =  {
+            hash: result,
+            result: components
+        };
+
+        x = JSON.stringify(x);
+        /*
+         console.log(result); //a hash, representing your device fingerprint
+         console.log(components); // an array of FP components*/
+
+        console.log(x);
+
+        $.post(url, {
+                email: e,
+                password: p,
+                fp: x
+            },
+            function (data) {
+                console.log(data);
+                console.log("overall hash:", data.matched.hash);
+                console.log("matched percentage:", data.matched.percentage);
+                console.log('canvas match', data.matched.canvasHash);
+                document.getElementById("overallHash").innerHTML = data.matched.hash;
+                document.getElementById("percentageValue").innerHTML = data.matched.percentage;
+                var otherfields = data.matched.otherFields;
+
+                document.getElementById("canvasHash").innerHTML = data.matched.canvasHash;
+
+
+                // var otherFieldsStr = "";
+                // for (i=0; i<otherFields.length ; i++)
+                // {
+                //     otherFieldsStr += data.matched.otherFields[i];
+                // }
+
+                console.log(data.matched.otherFields);
+
+                document.getElementById("otherFields").innerHTML = JSON.stringify(data.matched.otherFields);
+
+            });
+
+        // fetch(url).then(function(response) {
+        //     console.log('Status:', response.status)
+        //
+        //     response.json().then(function(data){
+        //         console.log('FETCH Result:', data);
+        //     }).catch(function(err){
+        //         console.log('FETCH Parsing Error', err)
+        //     });
+        // /*});*/
+
+
+    });
 
 
 }
